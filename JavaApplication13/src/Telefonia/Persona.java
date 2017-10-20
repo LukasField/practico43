d882e1043e2be14afa1b2aad0d3c9e7fe373320f
ULP <ULP@A6-PC18>
2017-10-20 16:26:33 -0300

    cel

---
+++ JavaApplication13/src/Telefonia/Persona.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Telefonia;
+/**
+ *
+ * @author ULP
+ */
+public class Persona {
+    
+    private long dni;
+    private String nombre;
+    private String commit;
+    public Persona(long dni, String nombre){
+        this.dni = dni;
+        this.nombre = nombre;
+    }
+    
+    public void llamarMensajear(Telefono tel, long nroDestino, String mensaje){
+        
+    }
+    
+    
+    
+    
+}
